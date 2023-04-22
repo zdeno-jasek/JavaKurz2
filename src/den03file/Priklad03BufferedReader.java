98af0f7aba534edab845763f6345a7a71b88b744
zdeno-jasek <32713775+zdeno-jasek@users.noreply.github.com>
2023-04-22 23:28:03 +0200

    den03

---
+++ src/den03file/Priklad03BufferedReader.java
@@ -0,0 +1,27 @@
+package den03file;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+// VYSVETLENIE
+public class Priklad03BufferedReader {
+
+	/*
+	 * Príklad na načítanie obsahu súboru s názvom "vystup.txt".
+	 * FileReader - umožňuje čítať súbor znak po znaku.
+	 * BufferedReader - umožňuje čítať Reader po riadkoch (nie súbor)
+	 */
+	public static void main(String[] args) {
+		
+		try (
+				FileReader subor = new FileReader("vystup.txt"); 
+				BufferedReader reader = new BufferedReader(subor);
+				) {
+			System.out.println( reader.readLine());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println( "Koniec" );
+	}
+}
