9341954f2e35afad1921fc92a4cbbad23ff032f4
zdeno-jasek <32713775+zdeno-jasek@users.noreply.github.com>
2023-04-26 17:15:08 +0200

    Meranie casu

--- src/den01bigdecimal/Priklad03BigDecimalSucetRiesenie.java
+++ src/den01bigdecimal/Priklad03BigDecimalSucetRiesenie.java
@@ -10,10 +10,15 @@ public class Priklad03BigDecimalSucetRiesenie {
 	 * Úlohou je sčítať prvých 100 čísel použitím BigDecimalu. 
 	 */
 	public static void main(String[] args) {
+		long zaciatok = System.currentTimeMillis();
+		
 		BigDecimal sumar = BigDecimal.ZERO;
-		for (int i = 1; i <= 100; i++) {
+		for (int i = 1; i <= 1000000; i++) {
 			sumar = sumar.add( BigDecimal.valueOf(i) );
 		}
+		
+		long koniec = System.currentTimeMillis();
+		System.out.println( koniec - zaciatok );
 		System.out.println( sumar );
 	}
 }
